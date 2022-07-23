235571a19cb215f4c6149d6186c4a423841fcabd
Du Spada <eduardo.spada@platformbuilders.io>
2022-07-23 13:45:24 -0300

    feat(sair): implement

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,20 @@
+import { useNavigation } from "@react-navigation/native";
 import styled from "styled-components/native";
 
 export default function Header() {
+  const navigation = useNavigation();
+
+  const onPress = () => navigation.navigate("STACK_PUBLIC");
+
   return (
     <Wrapper>
       <Title>Sowe</Title>
       <LocationWrapper>
         <Text>Bairro do Clima Bom</Text>
         <BoldText>Sorocaba, SP</BoldText>
+        <Link onPress={onPress}>
+          <LinkText>Sair</LinkText>
+        </Link>
       </LocationWrapper>
     </Wrapper>
   );
@@ -36,4 +44,11 @@ const Text = styled.Text`
 
 const BoldText = styled(Text)`
   font-weight: bold;
+`;
+
+const Link = styled.TouchableOpacity``;
+
+const LinkText = styled(Text)`
+  padding-top: 10px;
+  font-size: 12px;
 `;
